c05fafa66135049e007333325bc15962c0baf63f
Andoly <andoly.cs@hotmail.com>
2020-11-16 21:02:12 -0300

    Footer | Button com polimofismo | Ajuste

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,6 +1,5 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-import 'jest-styled-components'
 
 import Heading from '.'
 
@@ -20,7 +19,7 @@ describe('<Heading />', () => {
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
   it('should render a heading with a line at the bottom', () => {
