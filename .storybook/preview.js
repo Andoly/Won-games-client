5b8162886fcffe707d302d90c552d7608f95766a
Andoly <andoly.cs@hotmail.com>
2020-11-19 10:29:23 -0300

    Ajuste estrutura e background

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,8 +5,23 @@ import theme from '../src/styles/theme'
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyles removeBg />
       <Story />
     </ThemeProvider>
   )
 ]
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
