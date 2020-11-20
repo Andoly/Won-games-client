0f8591c1439474e1b67fe9dbe61ce0d90ac9f51f
Andoly <andoly.cs@hotmail.com>
2020-11-20 01:22:38 -0300

    matchMedia ...

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,13 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+/**
+ * fix: `matchMedia` not present, legacy browsers require a polyfill
+ */
+global.matchMedia = global.matchMedia || function() {
+  return {
+      matches : false,
+      addListener : function() {},
+      removeListener: function() {}
+  }
+}
