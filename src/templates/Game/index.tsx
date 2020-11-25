399f112df32a34b5765520513907128265e4307a
Andoly <andoly.cs@hotmail.com>
2020-11-25 13:58:43 -0300

    Game com GameInfo | Ajustes para rota

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,10 +1,23 @@
 import Base from 'templates/Base'
 
+import GameInfo, { GameInfoProps } from 'components/GameInfo'
+
 import * as S from './styles'
 
-const Game = () => (
+export type GameTemplateProps = {
+  cover: string
+  gameInfo: GameInfoProps
+}
+
+const Game = ({ cover, gameInfo }: GameTemplateProps) => (
   <Base>
-    <h1>Game</h1>
+    <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+    </S.Main>
   </Base>
 )
 
