7af9cfa5bcb6952582a3ff53666267b48a7306c3
Andoly <andoly.cs@hotmail.com>
2020-11-26 22:56:41 -0300

    Divider para usar em Wishlist

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -5,6 +5,7 @@ import Gallery, { GalleryImageProps } from 'components/Gallery'
 import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 
 import TextContent from 'components/TextContent'
+import { Divider } from 'components/Divider'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
@@ -49,6 +50,7 @@ const Game = ({
 
       <S.SectionGameDetails>
         <GameDetails {...details} />
+        <Divider />
       </S.SectionGameDetails>
 
       <Showcase
--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -3,6 +3,7 @@ import Base from 'templates/Base'
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
 import { Container } from 'components/Container'
+import { Divider } from 'components/Divider'
 import { Grid } from 'components/Grid'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
@@ -31,6 +32,7 @@ const Wishlist = ({
           <GameCard key={`wishlist-${index}`} {...game} />
         ))}
       </Grid>
+      <Divider />
     </Container>
 
     <Showcase
